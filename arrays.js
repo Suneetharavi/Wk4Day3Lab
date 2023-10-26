7bf050ed4f2f9437bbf7ddc70969941e2a17cdca
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-10-26 10:30:05 -0400

    Get Even answered

--- arrays.js
+++ arrays.js
@@ -1,3 +1,14 @@
+//Write a for loop that will log the numbers 1 through 20.
 for (let i=0;i<=20;i++){
     console.log(i)
 }
+// committed with message "Easy Going answered"
+console.log('///////////////////////////////////////////////////')
+//Write a for loop that will log only the even numbers in 0 through 200.
+console.log("The Even numbers from 0 to 200\n")
+for(let i=0;i<=200;i+=2){ 
+    console.log(i);
+}
+console.log('///////////////////////////////////////////////////')
+
+
